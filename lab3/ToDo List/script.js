bfed45346f608070746aefd44bd813a09059c395
SkyWhyWa1ker <157167473+SkyWhyWa1ker@users.noreply.github.com>
2025-02-17 12:55:07 +0500

    startмвмвм

--- lab3/ToDo List/script.js
+++ lab3/ToDo List/script.js
@@ -1,32 +1,36 @@
-
 let inputField = document.getElementById("task");
 
-//When pressed Add-button
-function addTask() { 
-    let taskText = inputField.value.trim(); 
-    if (taskText === "") return; 
- 
-    let li = document.createElement("li"); 
+
+inputField.addEventListener("input", function () {
+    this.value = this.value.replace(/[^a-zA-Z ]/g, "");
+});
+
+
+function addTask() {
+    let taskText = inputField.value.trim();
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
     li.innerHTML = `<input type="checkbox" onclick="toggleTask(this)"> 
                     <span class="task-text">${taskText}</span> 
-                    <button class="delete-btn" onclick="deleteTask(this)">🗑</button>`; 
-     
-    document.getElementById("task-list").appendChild(li); 
-    inputField.value = ""; 
-} 
- 
-//When pressed checkbox(task is done)
-function toggleTask(checkbox) { 
-    let taskText = checkbox.nextElementSibling; 
-    taskText.classList.toggle("completed", checkbox.checked); 
-} 
- 
-//When pressed delete icon
-function deleteTask(button) { 
-    button.parentElement.remove(); 
-} 
+                    <button class="delete-btn" onclick="deleteTask(this)">🗑</button>`;
 
-//When typed and pressed enter
+    document.getElementById("task-list").appendChild(li);
+    inputField.value = "";
+}
+
+
+function toggleTask(checkbox) {
+    let taskText = checkbox.nextElementSibling;
+    taskText.classList.toggle("completed", checkbox.checked);
+}
+
+
+function deleteTask(button) {
+    button.parentElement.remove();
+}
+
+
 inputField.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
         addTask();
